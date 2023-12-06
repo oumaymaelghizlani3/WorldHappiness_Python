b3c788253f0e648630aa49f09ab2ebb739e9102a
oumayma <oumaymaelghizlani3@gmail.com>
2023-12-06 19:56:39 +0100

    Le fichier est bien enregistré

--- Liste_Country.py
+++ Liste_Country.py
@@ -1,11 +1,10 @@
 import csv
 chemin_fichier_csv = 'WorldHappiness.csv'
-
 liste_de_listes = []
 
 with open(chemin_fichier_csv, newline='', encoding='utf-8') as fichier_csv:
     lecteur_csv = csv.DictReader(fichier_csv)
-
+    # Sauvegarder dans une liste de listes toutes ses données où le premier élément des sous listes est le « Country » les autres éléments contient toutes les informations du country.
     for ligne in lecteur_csv:
 
         sous_liste = [ligne["Country"]] + list(ligne.values())[1:]
